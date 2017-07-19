4456c872e101b6b15f3915717ddd92bfc13af099
paul santaria <psantaria@hotmail.com>
2017-07-19 17:45:20 -0500

    fail

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+var gulp = require('gulp')
+var postcss = require('gulp-postcss')
+var rucksack =require ('rucksack-css')
+var cssnested = require('postcss-nested')
+var cssnext = require('postcss-cssnext')
+var mixins =require('postcss-mixins')
+var atImport = require('postcss-import')
+var csswring = require ('csswring')
+var mqpacker = require('css-mqpacker')
+var lost = require('lost')
+
+var browserSync = require('browser-sync').create()
+
+//servidor de dsarrollo
+
+gulp.task('serve', function(){
+  browserSync.init( {
+    server: {
+      baseDir: './dist'
+    }
+  })
+})
+
+//tarea para programar css
+gulp.task('css', function(){
+
+  var processors = [
+    atImport(),
+    mixins(),
+    cssnested,
+    lost(),
+    rucksack(),
+    cssnext({browsers:['> 5%', 'ie 8'] }),
+    mqpacker,
+    csswring(),
+  ]
+
+  return gulp.src('./src/invie.css')
+    .pipe(postcss(processors))
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(browserSync.stream())
+})
+
+//tare para vigilar los cambios
+gulp.task('watch', function(){
+  gulp.watch('./src/*.css', ['css'])
+  gulp.watch('./dist/*.html').on('change', browserSync.reload)
+})
+
+gulp.task('default',['watch', 'serve'])
